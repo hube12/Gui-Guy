cf7cb14ca239d02e5def4940be61b803652e29fa
Neil <h66r10@gmail.com>
2020-01-06 10:33:09 +0100

    Basic setup

---
+++ src/line.cpp
@@ -0,0 +1,5 @@
+//
+// Created by r on 06/01/2020.
+//
+
+#include "line.h"
---
+++ src/line.h
@@ -0,0 +1,14 @@
+//
+// Created by r on 06/01/2020.
+//
+
+#ifndef GUIGUY_LINE_H
+#define GUIGUY_LINE_H
+
+
+class line {
+
+};
+
+
+#endif //GUIGUY_LINE_H
